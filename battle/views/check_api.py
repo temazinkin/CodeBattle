d9f007f175f47b3f8a4e3e49f53ba27e7294db64
Tema Zinkin <commit@azinkin.ru>
2022-01-26 23:50:15 +0300

    ref

--- battle/views/check_api.py
+++ battle/views/check_api.py
@@ -1,9 +1,10 @@
-import os
-
-import requests
 from django.shortcuts import render, get_object_or_404
 
 from battle.models import Solution
+from battle.services.check_system import (
+    request_result,
+    request_judge_id,
+)
 
 
 def check_solution(request, solution_id):
@@ -12,51 +13,30 @@ def check_solution(request, solution_id):
         id=solution_id,
     )
     if not solution.result and solution.judge_id:
-        try:
-            request_result = requests.post(
-                os.getenv('CHECK_SYSTEM'),
-                data={
-                    'SessionKey': os.getenv('CHECK_API'),
-                    'ActionNature': 'RunStatus',
-                    'ParcelID': solution.id,
-                    'RunID': solution.judge_id,
-                    'ContestID': solution.problem.contest_id,
-                }
-            )
-            if request_result.status_code == 200:
-                result_all = request_result.text.splitlines()[-1].split(';')
-                print(result_all)
-                result = result_all[6]
-                test_count = result_all[7]
-                if result:
-                    solution.result = result
-                if test_count:
-                    solution.test_count = int(test_count) - 1
-                solution.save()
-        except Exception as e:
-            print('RunStatus', e)
+        data = {
+            'ParcelID': solution.id,
+            'RunID': solution.judge_id,
+            'ContestID': solution.problem.contest_id,
+        }
+        result = request_result(data)
+        if result:
+            status, tests = result
+            solution.result = status
+            solution.test_count = tests
+            solution.save()
 
     if not solution.judge_id:
-        try:
-            request_judge_id = requests.post(
-                os.getenv('CHECK_SYSTEM'),
-                data={
-                    'SessionKey': os.getenv('CHECK_API'),
-                    'ActionNature': 'SubmitRun',
-                    'ParcelID': solution.id,
-                    'CodeContent': solution.code,
-                    'ContestID': solution.problem.contest_id,
-                    'ProblemShortName': solution.problem.short_name,
-                    'LanguageShortName': solution.lang,
-                }
-            )
-            if request_judge_id.status_code == 200:
-                judge_id = int(request_judge_id.text.splitlines()[-1])
-                if judge_id:
-                    solution.judge_id = judge_id
-                    solution.save()
-        except Exception as e:
-            print('SubmitRun', e)
+        data = {
+            'ParcelID': solution.id,
+            'CodeContent': solution.code,
+            'ContestID': solution.problem.contest_id,
+            'ProblemShortName': solution.problem.short_name,
+            'LanguageShortName': solution.lang,
+        }
+        judge_id = request_judge_id(data)
+        if judge_id:
+            solution.judge_id = judge_id
+            solution.save()
 
     context = {
         'title': 'Результат',
